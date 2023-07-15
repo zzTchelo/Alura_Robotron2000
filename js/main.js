6dee4f80a412db7a5dbadf8fd8b81d0fd25a7f9b
joao Marcelo <jmarcelo06@outlook.com>
2023-07-15 01:33:57 -0300

    Finalizando Projeto

--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
-const robotron = document.querySelector('#robotron');
+const classControle = document.querySelectorAll('[data-controle]')
+const statusRobo = document.querySelectorAll('[data-status]')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-const subtrair = document.querySelector('#subtrair');
-const somar = document.querySelector('#somar');
-const braco_robotron = document.querySelector('#braco_robotron');
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
-subtrair.addEventListener('click', () => {
-    braco_robotron.value --
-})
+classControle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+            iteracoes(evento.target.dataset.controle, evento.target.parentNode)
+            updateStatus(evento.target.dataset.peca)
+        })
+    }
+)
 
-somar.addEventListener('click', () => {
-    braco_robotron.value ++
-})
+function iteracoes (operacao, controleControlador) {
+    const controle = controleControlador.querySelector('[data-contador]')
+    
+    if (operacao === '+')
+        controle.value ++
+    else 
+        controle.value --
+}
 
-robotron.addEventListener('click', () => {
-    console.log('Oi, essa é uma Arrow Function')
-})
+function updateStatus(peca) {
+    statusRobo.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.status]
+    })
+}
